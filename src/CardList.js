012c908b11b78c9ea8645f17bb147547081e9a58
Mihir Shiradhonkar <mihir2911@gmail.com>
2023-07-07 12:24:12 +0530

    Initial Commit

--- src/CardList.js
+++ src/CardList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card';
 import {robots} from './robots';
-
+//
 const CardList=({robots}) => {
     const cardArray=robots.map((user,i)=>{
         return <Card 
